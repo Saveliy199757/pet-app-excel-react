06cba3d934acead8fdd659044e2dee863f7c7000
saveliyB <saveliy199757@gmail.com>
2023-01-21 10:33:14 +0300

    add interfaces for toolbar buttons, add view button

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,6 +1,62 @@
 import React from 'react';
 
+interface styleTextAlign {
+    textAlign: string
+}
+
+interface styleTextDecoration {
+    textDecoration: string
+}
+
+interface styleFontWeight {
+    fontWeight: string
+}
+
+interface styleFontStyle {
+    fontStyle: string
+}
+
+export interface IButtons {
+    icon: string;
+    active: boolean;
+    value: styleTextAlign | styleTextDecoration | styleFontWeight | styleFontStyle
+
+}
+
 const Toolbar = () => {
+    const state: any = [];
+    const buttons: IButtons[] = [
+        {
+            icon: 'format_align_left',
+            active: state['textAlign'] === 'left',
+            value: {textAlign: 'left'}
+        },
+        {
+            icon: 'format_align_center',
+            active: state['textAlign'] === 'center',
+            value: {textAlign: 'center'}
+        },
+        {
+            icon: 'format_align_right',
+            active: state['textAlign'] === 'right',
+            value: {textAlign: 'right'}
+        },
+        {
+            icon: 'format_bold',
+            active: state['fontWeight'] === 'bold',
+            value: {fontWeight: state['fontWeight'] === 'bold' ? 'normal' : 'bold'}
+        },
+        {
+            icon: 'format_italic',
+            active: state['fontStyle'] === 'italic',
+            value: {fontStyle: state['fontStyle'] === 'italic' ? 'normal' : 'italic'}
+        },
+        {
+            icon: 'format_underlined',
+            active: state['textDecoration'] === 'underline',
+            value: {textDecoration: state['textDecoration'] === 'underline' ? 'none' : 'underline'}
+        }
+    ]
     return (
         <div>
             
--- src/components/Toolbar/components/Button.tsx
+++ src/components/Toolbar/components/Button.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import {IButtons} from "../Toolbar";
 
-const Button = () => {
+const Button = ({ active, value, icon }: IButtons) => {
+    const meta = `
+    data-type="button"
+    data-value='${JSON.stringify(value)}'
+  `
     return (
-        <div>
-            
+        <div
+            className={"button" + active ? 'active' : ''}
+            data-type="button"
+            data-value={JSON.stringify(value)}
+        >
+            <i
+                className="material-icons"
+                data-type="button"
+                data-value={JSON.stringify(value)}
+            >${icon}</i>
         </div>
     );
 };
