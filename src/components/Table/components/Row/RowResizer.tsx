057cd6ad81b4d78d5041ccc1009b9dd218afc05b
saveliyB <saveliy199757@gmail.com>
2023-01-25 13:05:21 +0300

    black code for test component RowResizer

--- src/components/Table/components/Row/RowResizer.tsx
+++ src/components/Table/components/Row/RowResizer.tsx
@@ -2,7 +2,23 @@ import React, {useState} from "react";
 import {DEFAULT_ROW_INFO_WIDTH} from "../../../../constans/table.constans";
 
 const RowResizer = () => {
-    const [width, setWidth] = useState<number | string>()
+    const [width, setWidth] = useState<number>()
+    const [position, setPosition] = useState({
+        y: 50,
+        coords: {} as any,
+    });
+    const handleMouseMove = (e: any) => {
+        setPosition(position => {
+
+            const yDiff = position.coords?.y - e.pageY;
+            return {
+                y: position.y - yDiff,
+                coords: {
+                    y: e.pageY,
+                },
+            };
+        });
+    }
     return (
         <div
             className="row-resize"
@@ -10,6 +26,10 @@ const RowResizer = () => {
             style={{width: width + "px"}}
             onMouseDown={() => setWidth(5000) }
             onMouseUp={() => setWidth(DEFAULT_ROW_INFO_WIDTH - 1)}
+            onMouseMove={(event) => console.log({
+                "button": event.button,
+                "event": event
+            })}
         >
         </div>
     );
