3d6def1906026b8782a2f0fb72c814efa8901f3d
saveliyB <saveliy199757@gmail.com>
2023-01-27 15:27:34 +0300

    start add logic row resize

--- src/components/Table/components/Row/RowResizer.tsx
+++ src/components/Table/components/Row/RowResizer.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { DEFAULT_ROW_INFO_WIDTH } from "../../../../constans/table.constans";
 
 interface IProps {
@@ -6,36 +6,40 @@ interface IProps {
 }
 
 const RowResizer = ({ setRowHeight }: IProps) => {
-  const ref = useRef(null);
-  const [width, setWidth] = useState<number>();
-  const [position, setPosition] = useState({
-    y: 50,
-    coords: {} as any,
-  });
-  const handleMouseMove = (e: any) => {
-    setPosition((position) => {
-      const yDiff = position.coords?.y - e.pageY;
-      return {
-        y: position.y - yDiff,
-        coords: {
-          y: e.pageY,
-        },
-      };
-    });
-  };
-  const getRec = () => {
-    const element = ref.current;
-    console.log();
-  };
+  const ref = useRef<any>(null);
+  const [width, setWidth] = useState<number>(0);
+  const [isMouseDown, setIsMouseDown] = useState<boolean>(false);
+  const [bottom, setBottom] = useState<number>(0);
+  const [opacity, setOpacity] = useState(0);
   return (
     <div
       className="row-resize"
       data-resize="row"
       ref={ref}
-      style={{ width: width + "px" }}
-      onMouseDown={() => setRowHeight(100)}
-      onMouseUp={() => setWidth(DEFAULT_ROW_INFO_WIDTH - 1)}
-      onMouseMove={(event) => console.log()}
+      style={{ opacity: opacity, right: width + "px", bottom: bottom + "px" }}
+      onMouseDown={() => {
+        setIsMouseDown(true);
+        setWidth(-5000);
+        setOpacity(1);
+      }}
+      onMouseUp={() => {
+        setWidth(DEFAULT_ROW_INFO_WIDTH - 1);
+        setIsMouseDown(false);
+        setOpacity(0);
+      }}
+      onMouseMove={(event) => {
+        if (isMouseDown) {
+          const delta =
+            event.clientY -
+            Math.round(ref.current.getBoundingClientRect().bottom);
+          console.log({
+            delta,
+            bottom: ref.current.getBoundingClientRect().bottom,
+            clientY: event.clientY,
+          });
+          setBottom(-delta);
+        }
+      }}
     ></div>
   );
 };
